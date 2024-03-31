951de56ccec83c4c14a44ef8276bb94acd150dcc
Wesley <wesleyadwebster@gmail.com>
2024-03-31 00:16:37 -0500

    Added variables, using NextJS Image component, fixed Header

--- src/components/NavigationHeader/NavigationHeader.tsx
+++ src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import logoImg from '@/assets/logo.png';
 import classes from './NavigationHeader.module.scss';
 
-const {navLink} = classes;
+const { navLink, siteHeader, siteLogoText } = classes;
 
 const NavigationHeader = () => {
 
@@ -12,33 +14,46 @@ const NavigationHeader = () => {
             path: "/"
         },
         {
-            page: "Community",
+            page: "Foodies Community",
             path: '/community'
         },
         {
-            page: "Meals",
+            page: "Browse Meals",
             path: '/meals'
         },
-        {
-            page: "Share Meal",
-            path: '/meals/share'
-        }
+        // {
+        //     page: "Share Meal",
+        //     path: '/meals/share'
+        // }
     ];
 
     return (
-        <nav>
-            {availablePages.map((page) => {
-                return (
-                    <Link 
-                        className={navLink} 
-                        href={page.path} 
-                        key={page.path}>
-                            {page.page}
-                    </Link>
-                )
-            })}
-        </nav>
+        <section className={siteHeader}>
+            <Link href='/'>
+                <div className={siteLogoText}>
+                    <Image 
+                        priority
+                        width={75}
+                        height={75}
+                        src={logoImg} 
+                        alt="A plate with food on it" />
+                    <h1>NextLevel Foods</h1>
+                </div>
+            </Link>
 
+            <nav>
+                {availablePages.map((page) => {
+                    return (
+                        <Link
+                            className={navLink}
+                            href={page.path}
+                            key={page.path}>
+                            {page.page}
+                        </Link>
+                    )
+                })}
+            </nav>
+        </section>
     );
 };
 
