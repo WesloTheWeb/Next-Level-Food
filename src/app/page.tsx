6bfb418781d68db3062faaf26a36bbed449923d4
Wesley <wesleyadwebster@gmail.com>
2024-03-31 12:54:16 -0500

    stabilized styles, added in content for the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,51 @@
-import NavigationHeader from '@/components/NavigationHeader/NavigationHeader';
-import React from 'react';
+import Link from 'next/link';
+import classes from './page.module.scss';
 
-const Home = () => {
+const { header, slideshow, hero, cta, instructionalSectionBlurb } = classes;
+
+export default function Home({}) {
   return (
     <>
-      <p>🔥 Let's get started! 🔥</p>
+      <header className={header}>
+        <div className={slideshow}></div>
+        <section>
+          <div className={hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </section>
+      </header>
+
+      <main>
+        <section className={instructionalSectionBlurb}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+        <section className={instructionalSectionBlurb}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
     </>
   );
 }
-
-export default Home;
