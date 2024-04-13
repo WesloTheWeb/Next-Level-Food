f3287ea54cab7d4431f68ba4b9ff0d3ece812100
Wesley Webster <wesleyadwebster@gmail.com>
2024-04-13 03:01:45 -0500

    setting up imagePicker component

--- src/components/Meals/ImagePicker.tsx
+++ src/components/Meals/ImagePicker.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+'use client';
+
+import { useState, useRef } from "react";
 import classes from './ImagePicker.module.scss';
 
-const { } = classes;
+const { button, controls, input, picker } = classes;
 
 interface ImagePickerProps {
     label: string,
@@ -9,17 +11,31 @@ interface ImagePickerProps {
 }
 
 const ImagePicker = ({ label, name }: ImagePickerProps) => {
+    const {pickedImage, setPickedImage} = useState()
+
+    const imageInput = useRef<HTMLInputElement>(null)   ;
+
+    const handlePickClick = () => {
+        if (imageInput.current) {
+            imageInput.current.click();
+        };
+    };
 
     return (
-        <div className={classes.picker}>
+        <div className={picker}>
             <label htmlFor={name}>{label}</label>
-            <section className={classes.controls}>
+            <section className={controls}>
                 <input
+                    className={input}
                     type="file"
                     id={name}
                     accept="image/png, image/jpeg, image/jpg"
                     name={name}
+                    ref={imageInput}
                 />
+                <button className={button} type="button" onClick={handlePickClick}>
+                    Pick an Image
+                </button>
             </section>
         </div>
     );
