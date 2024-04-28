109a718f3ef90c3464da370073a736eede785297
Wesley <wesleyadwebster@gmail.com>
2024-04-27 23:11:02 -0500

    Meal form is now its own component

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -1,8 +1,7 @@
 // 'use client';
 
 import classes from './page.module.scss';
-import ImagePicker from "@/components/Meals/ImagePicker";
-import { shareMeal } from "../../../../lib/actions";
+import MealForm from '@/components/Meals/MealForm';
 
 export default function ShareMealPage() {
 
@@ -15,39 +14,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
-          <div className={classes.row}>
-            <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
-            </p>
-            <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
-            </p>
-          </div>
-          <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
-          </p>
-          <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
-          </p>
-          <p>
-            <label htmlFor="instructions">Instructions</label>
-            <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
-              required
-            ></textarea>
-          </p>
-          <ImagePicker label="Your image" name="image" />
-          <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
-          </p>
-        </form>
+        <MealForm />
       </main>
     </>
   );
---
+++ src/components/Meals/MealForm.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ImagePicker from './ImagePicker';
+import { shareMeal } from '../../../lib/actions';
+import classes from './MealForm.module.scss';
+
+const MealForm = () => {
+    return (
+        <form className={classes.form} action={shareMeal}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          <ImagePicker label="Your image" name="image" />
+          <p className={classes.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+    );
+};
+
+export default MealForm;
