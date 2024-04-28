e5855fc9edc22d5384462f3b46f9cdfe3eb2cac9
Wesley <wesleyadwebster@gmail.com>
2024-04-28 14:32:39 -0500

    Added custom error page, reinforced type checking

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,18 +3,47 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+type Meal = {
+    title: string | null;
+    summary: string | null;
+    instructions: string | null;
+    image: File | null;
+    creator: string | null;
+    creator_email: string | null;
+    id?: string; // optional property
+    slug?: string; // optional property
+};
+
 export async function shareMeal(formData: FormData) {
+
+    const isInvalidText = (text: string) => {
+        return !text || text.trim() === '';
+    };
     
-    const meal = {
-        title: formData.get('title'),
-        summary: formData.get('summary'),
-        instructions: formData.get('instructions'),
-        image: formData.get('image'),
-        creator: formData.get('name'), 
-        creator_email: formData.get('email')
+    const meal: Meal = {
+        title: formData.get('title') as string,
+        summary: formData.get('summary') as string,
+        instructions: formData.get('instructions') as string,
+        image: formData.get('image') as File | null,
+        creator: formData.get('name') as string, 
+        creator_email: formData.get('email') as string
     };
 
     console.log('meal', meal);
+
+    if (
+        (meal.title && isInvalidText(meal.title)) || 
+        (meal.summary && isInvalidText(meal.summary)) || 
+        (meal.instructions && isInvalidText(meal.instructions)) ||
+        (meal.creator && isInvalidText(meal.creator)) ||
+        (meal.creator_email && isInvalidText(meal.creator_email)) ||
+        // Check if creator_email is a string and includes an '@'
+        (typeof meal.creator_email !== 'string' || !meal.creator_email.includes('@')) ||
+        // Check if image is a File and has a size greater than 0
+        (!(meal.image instanceof File) || meal.image.size === 0)
+    ) {
+        throw new Error('Invalid input')
+    }
 
     await saveMeal(meal);
     redirect('/meals');
---
+++ src/app/meals/share/error.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from "react";
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>Ruh - roh! An error occurred!</h1>
+            <p>Failed to create meal.</p>
+        </main>
+    )
+};
